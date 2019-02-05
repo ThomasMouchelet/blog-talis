9daba0998c405c9cbcad7eb9cb426ee3aabe914c
thomasM <mouchelet.thomas@gmail.com>
2019-02-05 10:44:41 +0100

    create header file

---
+++ add_post.php
@@ -0,0 +1,9 @@
+<?php
+require "header.php";
+?>
+    <form action="">
+        <input type="text" name="Title" placeholder="title">
+
+    </form>
+</body>
+</html>
--- admin.php
+++ admin.php
@@ -1,21 +1,9 @@
 <?php
-session_start();
-if(!isset($_SESSION["pseudo"])){
-    header("Location: login.php");
-}
+require 'header.php'; 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php require_once 'nav-bar.php'; ?>
-
     <h1>Admin</h1>
+
+    <a href="add_post.php">Ajouter article</a>
 
 </body>
 </html>
