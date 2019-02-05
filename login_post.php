509cac9d85a8bed5cb8de6571dc10badb6748ee6
thomasM <mouchelet.thomas@gmail.com>
2019-02-05 10:06:05 +0100

    database login

--- login_post.php
+++ login_post.php
@@ -1,11 +1,16 @@
 <?php
-// Si pseudo égale à admin et password égale à admin
-if($_POST["pseudo"] === "admin" && $_POST["password"] === "admin") {
+require "database.php";
+
+$req = $db->prepare('SELECT * FROM users WHERE pseudo = :pseudo AND password = :password');
+$req->execute(array(
+    "pseudo" => $_POST["pseudo"],
+    "password" => $_POST["password"]
+));
+$result = $req->fetch();
+if(!$result){
+    header("Location: login.php");
+}else{
     session_start();
     $_SESSION["pseudo"] = $_POST["pseudo"];
     header("Location: admin.php");
-}else if($_POST["password"] !== "admin"){
-    header("Location: login.php?message=Erreur de password");
-}else if($_POST["pseudo"] !== "admin"){
-    header("Location: login.php?message=Erreur de pseudo");
 }
