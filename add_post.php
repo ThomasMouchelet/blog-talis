7dbd9f4115f3a9b882b6e512c828469b5cb0b390
thomasM <mouchelet.thomas@gmail.com>
2019-02-05 10:58:54 +0100

    add posts 1

--- add_post.php
+++ add_post.php
@@ -1,9 +1,10 @@
 <?php
 require "header.php";
 ?>
-    <form action="">
-        <input type="text" name="Title" placeholder="title">
-
+    <form action="add_post_post.php" method="post">
+        <input type="text" name="title" placeholder="Title">
+        <textarea name="content" placeholder="Content"></textarea>
+        <input type="submit">
     </form>
 </body>
 </html>
---
+++ add_post_post.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+require "database.php";
+
+// prepare REQ
+// Execute REQ
+// Redirect to admin + message
