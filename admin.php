450a62c0050b217c80030842f971f6c25fe78f63
thomasM <mouchelet.thomas@gmail.com>
2019-02-05 15:48:55 +0100

    add DELETE posts

--- admin.php
+++ admin.php
@@ -8,5 +8,16 @@
 
     <a href="add_post.php">Ajouter article</a>
 
+    <?php
+    $req = $db->query("SELECT id, title FROM posts");
+    $req->execute();
+
+    while($row = $req->fetch(PDO::FETCH_ASSOC)){
+        echo "<h4>{$row["title"]}</h4>";
+        echo "<a href='delete_post.php?id={$row["id"]}'>supprimer</a>";
+    }
+
+    ?>
+
 </body>
 </html>
